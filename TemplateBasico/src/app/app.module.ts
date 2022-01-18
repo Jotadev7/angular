ca82496dec6437887e71086ad0be9cad5321d8ba
Joao <jotadev7@gmail.com>
2022-01-18 15:18:54 -0300

    Angular Avançado - Angular Reactive Forms (Template)

--- TemplateBasico/src/app/app.module.ts
+++ TemplateBasico/src/app/app.module.ts
@@ -1,30 +1,31 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule }   from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { SobreComponent } from './institucional/sobre/sobre.component';
-import { FooterComponent } from './navegacao/footer/footer.component';
-import { HomeComponent } from './navegacao/home/home.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
 import { rootRouterConfig } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SobreComponent,
-    FooterComponent,
+    MenuComponent,
     HomeComponent,
-    MenuComponent
+    FooterComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    [RouterModule.forRoot(rootRouterConfig, {useHash: false})],
+    // RouterModule,
+    [RouterModule.forRoot(rootRouterConfig, { useHash: false})],
   ],
   providers: [
     {provide: APP_BASE_HREF, useValue: '/'}
