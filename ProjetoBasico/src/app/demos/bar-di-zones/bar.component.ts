cdd976360befb01cc911458cf55fdb7ba56a74d1
Joao <jotadev7@gmail.com>
2022-01-21 17:25:13 -0300

    Angular Avançado - NgModules (Injection Token)

--- ProjetoBasico/src/app/demos/bar-di-zones/bar.component.ts
+++ ProjetoBasico/src/app/demos/bar-di-zones/bar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Inject, OnInit } from "@angular/core";
+import { BarUnidadeConfig, BAR_UNIDADE_CONFIG } from "./bar.config";
 import { BarServices, BarServicesMock } from "./bar.service";
 
 @Component({
@@ -11,10 +12,18 @@ import { BarServices, BarServicesMock } from "./bar.service";
 export class BarComponent implements OnInit {
 
     barBebida1: string = '';
+    Config: BarUnidadeConfig = {unidadeId: 1, unidadeToken: '123'};
+    ConfigManual: BarUnidadeConfig = {unidadeId: 1, unidadeToken: '123'};
 
-    constructor(private barServices: BarServices){ }
+    constructor(
+        private barServices: BarServices,
+        @Inject('ConfigManualUnidade') private ApiConfigManual: BarUnidadeConfig,
+        @Inject(BAR_UNIDADE_CONFIG) private ApiConfig: BarUnidadeConfig
+        ){ }
 
     ngOnInit(): void {
         this.barBebida1 = this.barServices.obterBebidas();
+        this.ConfigManual = this.ApiConfigManual;
+        this.Config = this.ApiConfig;
     }
 }
