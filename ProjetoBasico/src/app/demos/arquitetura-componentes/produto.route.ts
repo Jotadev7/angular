daf965fc6f9dac249eaada7507fdf777468a6ebe
Joao <jotadev7@gmail.com>
2022-01-21 14:43:06 -0300

    Angular Avançado - Obtendo dados da rota

--- ProjetoBasico/src/app/demos/arquitetura-componentes/produto.route.ts
+++ ProjetoBasico/src/app/demos/arquitetura-componentes/produto.route.ts
@@ -8,7 +8,7 @@ import { ProdutosResolve } from "./services/produto.resolve";
 const produtoRouterConfig: Routes = [
     {path: '', component: ProdutoAppComponent, children: [
         {path: '', redirectTo: 'todos'},
-        {path: ':estado', component: ProdutoDashboardComponent, resolve: {produtos: ProdutosResolve}},
+        {path: ':estado', component: ProdutoDashboardComponent, resolve: {produtos: ProdutosResolve}, data: {teste: 'informação'}},
         {path: 'editar/:id', component: EditarProdutoComponent}
     ]},
 ];
