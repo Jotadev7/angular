56b10726c9043fde5c85395f01da1a9a18ad6fed
Joao <jotadev7@gmail.com>
2022-01-26 15:42:45 -0300

    Angular Avançado - Projeto Final (Implementação cadastro de usuário - Validações)

--- ProjetoFinal/front-end/src/app/conta/conta.module.ts
+++ ProjetoFinal/front-end/src/app/conta/conta.module.ts
@@ -9,6 +9,9 @@ import { LoginComponent } from './login/login.component';
 import { ContaAppComponent } from './conta.app.component';
 
 import { ContaRoutingModule } from './conta.route';
+import { ContaService } from './services/conta.service';
+
+import { CustomFormsModule } from 'ng2-validation'
 
 
 @NgModule({
@@ -23,7 +26,11 @@ import { ContaRoutingModule } from './conta.route';
     ContaRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    CustomFormsModule
+  ],
+  providers: [
+    ContaService
   ]
 })
 export class ContaModule { }
