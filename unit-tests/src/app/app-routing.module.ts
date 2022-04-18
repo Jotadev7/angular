4920199941f96f8766d8470f6ec4121bf112fe9c
Joao <jotadev7@gmail.com>
2022-04-18 15:11:04 -0300

    Criando novo componente para testes, análise code-coverage

--- unit-tests/src/app/app-routing.module.ts
+++ unit-tests/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContadorComponent } from './contador/contador.component';
+import { TestesComponent } from './testes/testes.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/contador', pathMatch: 'full'},
-  {path: 'contador', component: ContadorComponent}
+  {path: 'contador', component: ContadorComponent},
+  {path: 'testes', component: TestesComponent}
 ];
 
 @NgModule({
